5533034ba4d800bbd4a86e74ec5e711969a2b6ff
NX <798928604@qq.com>
2019-04-11 13:28:01 +0800

    修改login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,7 +1,30 @@
 import './login.less'
 import '../../fonts/icon/iconfont.css'
 import { tip } from '../../components/tips/tip'
+import { Request } from '../../class/ajax'
+const _Request = new Request();
+const submit = document.getElementsByClassName('submit-button')[0];
 if(process.env.NODE_ENV == 'development'){
     const container = document.getElementsByClassName('login-container')[0];
-    container.append(tip(`${process.env.NODE_ENV}:随便输入账号密码！`));
+    const _tip = tip({
+        text:`${process.env.NODE_ENV}:随便输入账号密码！`
+    });
+    container.append(_tip);
+    setTimeout(()=>{
+        container.removeChild(_tip)
+    },4000)
 }
+submit.addEventListener('click',(e) => {
+    const _e = e || window.event;
+    _e.preventDefault();
+    _Request.post({
+        url:'api/login',
+        data:document.getElementById('login-form'),
+        sucess:(res)=>{
+            console.log(res)
+        },
+        complete:(res)=>{
+            console.log(res)
+        }
+    })
+})
