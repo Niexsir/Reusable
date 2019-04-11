8a898ae55d995ef12ff8bb87b8dc0082fb3f72c5
NX <798928604@qq.com>
2019-04-11 13:27:29 +0800

    增加tip小箭头方向

--- src/components/tips/tip.js
+++ src/components/tips/tip.js
@@ -1,7 +1,7 @@
 import "./tip.less";
-const tip = (text='tip') => {
+const tip = ({text='tip',position='top'}) => {
     const _ele = document.createElement('div');
-    _ele.innerHTML = `<div class='tips'>${text}</div>`;
+    _ele.innerHTML = `<div class='tips tip-${position}'>${text}</div>`;
     return _ele.children[0]
 }
 export { tip }
