fb2ec137558729c704bd66f3e813c88419d57336
NX <798928604@qq.com>
2019-04-16 22:09:39 +0800

    备注修改，增加阴影

--- src/components/tips/tip.js
+++ src/components/tips/tip.js
@@ -1,4 +1,8 @@
 import "./tip.less";
+/**
+ * @param {String} position tip的出现的位置 top bottom left right 
+ * @param {String} text 展示的文字 
+ */
 const tip = ({text='tip',position='top'}) => {
     const _ele = document.createElement('div');
     _ele.innerHTML = `<div class='tips tip-${position}'>${text}</div>`;
